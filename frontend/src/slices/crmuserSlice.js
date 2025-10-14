19dd08f4aad2b6df38d8ba5f1cf78cc52d8fdecd
Rukshanda <rukshandanadir@gmail.com>
2025-10-14 13:03:43 +0500

    new-changes

--- frontend/src/slices/crmuserSlice.js
+++ frontend/src/slices/crmuserSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL;
 const TOKEN = import.meta.env.VITE_ADMIN_TOKEN;
 
 // ✅ Fetch all CRM users
